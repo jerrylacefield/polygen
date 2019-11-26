47075a9683d1177af551260023b4b7e9030a395d
Jerry Lacefield <jerry.lacefield@gmail.com>
2019-11-25 22:07:59 -0600

    cleaned up main.py file

--- main.py
+++ main.py
@@ -12,26 +12,6 @@ for i in range(3):
     points_list.append(PG.Point())
     points_list[i].set_point(plane)
     points_list[i].insert_point(plane, points, points_list[i])
-    
-# for i in points_list:
-#     print(points_list[i])
-
-# p1 = PG.Point()
-# p2 = PG.Point()
-# p3 = PG.Point()
-#
-# p1.set_point(plane)
-# p1.insert_point(plane, points, p1)
-#
-# p2.set_point(plane)
-# p2.insert_point(plane, points, p2)
-#
-# p3.set_point(plane)
-# p3.insert_point(plane, points, p3)
-#
-# p1.print_point('A')
-# p2.print_point('B')
-# p3.print_point('C')
 
 print(points)
 
